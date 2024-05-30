94fe5e2dd1a712ca3e11c2e539a609869f4a626f
imanfsx <iman30039581@gmail.com>
2024-05-30 21:12:59 +1000

    full upload of code

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+import "./Footer.css";
+
+export default function App() {
+  return (
+    <div className="App text-white">
+      <div className="container">
+        <Weather defaultCity="Sydney" />
+
+        <footer className="footer">
+          {" "}
+          {}
+          This project was coded with 💗 by{" "}
+          <a
+            href="https://www.github.com/imansfx"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Iman Sohail
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/imanfsx"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            available on GitHub
+          </a>{" "}
+          and{" "}
+          <a href="" target="_blank" rel="noopener noreferrer">
+            hosted on Vercel
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
